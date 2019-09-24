1cec5aa881ae81be7630bda922f1c545e6fbc3ae
moralesm90 <stargamer39@gmail.com>
2019-09-24 12:29:07 -0500

    added student button

--- GroupProject/GroupProject/Student.h
+++ GroupProject/GroupProject/Student.h
@@ -34,6 +34,8 @@ namespace GroupProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  button1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,11 +50,31 @@ namespace GroupProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Student";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(78)), static_cast<System::Int32>(static_cast<System::Byte>(184)),
+				static_cast<System::Int32>(static_cast<System::Byte>(206)));
+			this->button1->Location = System::Drawing::Point(59, 93);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(142, 34);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = false;
+			// 
+			// Student
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->button1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Name = L"Student";
+			this->Text = L"Student";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
