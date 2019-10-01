43a299470af7bb8058246166d646a6e47a6aeeb3
florine <flonswal@gmail.com>
2019-10-01 12:54:35 -0500

    add different buttons

--- GroupProject/GroupProject/admin.h
+++ GroupProject/GroupProject/admin.h
@@ -36,6 +36,8 @@ namespace GroupProject {
 		}
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button2;
 
 	protected:
 
@@ -52,9 +54,11 @@ namespace GroupProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(admin::typeid));
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(admin::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -62,10 +66,10 @@ namespace GroupProject {
 			// 
 			this->button1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(78)), static_cast<System::Int32>(static_cast<System::Byte>(184)),
 				static_cast<System::Int32>(static_cast<System::Byte>(206)));
-			this->button1->Location = System::Drawing::Point(91, 135);
-			this->button1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->button1->Location = System::Drawing::Point(111, 128);
+			this->button1->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(119, 68);
+			this->button1->Size = System::Drawing::Size(159, 84);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"button1";
 			this->button1->UseVisualStyleBackColor = false;
@@ -73,22 +77,51 @@ namespace GroupProject {
 			// pictureBox1
 			// 
 			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(70, 25);
+			this->pictureBox1->Location = System::Drawing::Point(93, 31);
+			this->pictureBox1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(166, 60);
+			this->pictureBox1->Size = System::Drawing::Size(221, 74);
 			this->pictureBox1->TabIndex = 5;
 			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &admin::PictureBox1_Click);
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(78)), static_cast<System::Int32>(static_cast<System::Byte>(184)),
+				static_cast<System::Int32>(static_cast<System::Byte>(206)));
+			this->button3->Location = System::Drawing::Point(111, 315);
+			this->button3->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(159, 84);
+			this->button3->TabIndex = 7;
+			this->button3->Text = L"button3";
+			this->button3->UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(78)), static_cast<System::Int32>(static_cast<System::Byte>(184)),
+				static_cast<System::Int32>(static_cast<System::Byte>(206)));
+			this->button2->Location = System::Drawing::Point(111, 227);
+			this->button2->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(159, 84);
+			this->button2->TabIndex = 8;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &admin::Button2_Click_1);
 			// 
 			// admin
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(34)), static_cast<System::Int32>(static_cast<System::Byte>(36)),
 				static_cast<System::Int32>(static_cast<System::Byte>(49)));
-			this->ClientSize = System::Drawing::Size(320, 412);
+			this->ClientSize = System::Drawing::Size(427, 507);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button3);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->button1);
-			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->Name = L"admin";
 			this->Text = L"admin";
 			this->Load += gcnew System::EventHandler(this, &admin::Admin_Load);
@@ -101,5 +134,9 @@ namespace GroupProject {
 	}
 	private: System::Void Button2_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	private: System::Void Button2_Click_1(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void PictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
