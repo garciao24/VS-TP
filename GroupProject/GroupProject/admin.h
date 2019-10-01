92b04eff5bbef36be56fe9c12679b64d90bd6955
moralesm90 <stargamer39@gmail.com>
2019-10-01 12:40:27 -0500

    added uhd logo at admin window

--- GroupProject/GroupProject/admin.h
+++ GroupProject/GroupProject/admin.h
@@ -35,7 +35,8 @@ namespace GroupProject {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+
 	protected:
 
 	private:
@@ -51,42 +52,47 @@ namespace GroupProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(admin::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
 			this->button1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(78)), static_cast<System::Int32>(static_cast<System::Byte>(184)),
 				static_cast<System::Int32>(static_cast<System::Byte>(206)));
-			this->button1->Location = System::Drawing::Point(42, 74);
+			this->button1->Location = System::Drawing::Point(91, 135);
+			this->button1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(159, 84);
+			this->button1->Size = System::Drawing::Size(119, 68);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"button1";
 			this->button1->UseVisualStyleBackColor = false;
 			// 
-			// button2
+			// pictureBox1
 			// 
-			this->button2->Location = System::Drawing::Point(86, 12);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(141, 35);
-			this->button2->TabIndex = 1;
-			this->button2->UseVisualStyleBackColor = true;
-			this->button2->Click += gcnew System::EventHandler(this, &admin::Button2_Click);
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(70, 25);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(166, 60);
+			this->pictureBox1->TabIndex = 5;
+			this->pictureBox1->TabStop = false;
 			// 
 			// admin
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(34)), static_cast<System::Int32>(static_cast<System::Byte>(36)),
 				static_cast<System::Int32>(static_cast<System::Byte>(49)));
-			this->ClientSize = System::Drawing::Size(282, 253);
-			this->Controls->Add(this->button2);
+			this->ClientSize = System::Drawing::Size(320, 412);
+			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->button1);
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->Name = L"admin";
 			this->Text = L"admin";
 			this->Load += gcnew System::EventHandler(this, &admin::Admin_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 
 		}
