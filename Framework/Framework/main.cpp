68a6e878716960051a0f68fc1feab63b0e6e66f8
Oscar Garcia <ogarcia2834@outlook.com>
2019-10-31 12:26:30 -0500

    vector schedule added

--- Framework/Framework/main.cpp
+++ Framework/Framework/main.cpp
@@ -3,11 +3,82 @@
 #include <string>
 using namespace std;
 
+struct schedule
+{
+	schedule(string bi, unsigned int io, unsigned int ni, double pi) :classname(bi), grade1(io), grade2(ni), grade3(pi) {}
+	string classname;
+	int classnum;
+	int grade1;
+	int grade2;
+	int grade3;
+
+};
+
+
+
+
+void input()
+{
+	system("cls");
+	string fname;
+	string lname;
+	int age;
+	int stdID;
+	int count = 0;
+
+	cout << "input first name";
+	cin >> fname;
+	cout << "input last name";
+	cin >> lname;
+	cout << "input student age";
+	cin >> age;
+	//cout << "input student date of birth";
+	//cin >> date;
+	cout << "enter student id";
+	cin >> stdID;
+
+	string stdID2 = to_string(stdID);
+	stdID2.append(".txt");
+
+	ofstream newStudent(stdID2);
+	newStudent << fname << " " << lname << " " << stdID << " " << age <<""<<count<<endl;
+	newStudent.close();
+
+}
+
+void search()
+{
+	system("cls");
+	string fname;
+	string lname;
+	int age;
+	int stdID;
+
+	cout << "enter student id"; 
+	cin >> stdID;
+	string stdID2 = to_string(stdID);
+	stdID2.append(".txt");
+
+
+
+
+}
+
+void edit()
+{
+
+}
+
+void display()
+{
+
+}
+
 int main()
 {
 	system("cls");
 	system("title Database");
-	system("color 0f");
+	//system("color 0f");
 	cout << "Please enter choice"<<endl;
 	cout << "1. input new student\n";
 	cout << "2. search student by id\n";
@@ -37,58 +108,4 @@ int main()
 	default:
 		break;
 	}
-}
-
-void input()
-{
-	system("cls");
-	string fname;
-	string lname;
-	int age;
-	int stdID;
-
-	cout << "input first name";
-	cin >> fname;
-	cout << "input last name";
-	cin >> lname;
-	cout << "input student age";
-	cin >> age;
-	//cout << "input student date of birth";
-	//cin >> date;
-	cout << "enter student id";
-	cin >> stdID;
-
-	string stdID2 = to_string(stdID);
-	stdID2.append(".txt");
-
-	ofstream newStudent(stdID2);
-	newStudent << fname << " " << lname << " " << stdID << " " << age <<endl;
-	newStudent.close;
-
-}
-
-void search()
-{
-	system("cls");
-	string fname;
-	string lname;
-	int age;
-	int stdID;
-
-	cout << "enter student id"; 
-	cin >> stdID;
-	string stdID2 = to_string(stdID);
-	stdID2.append(".txt");
-
-
-}
-
-void edit()
-{
-
-}
-
-void display()
-{
-
 }
