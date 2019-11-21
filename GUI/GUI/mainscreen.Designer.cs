e01841c7a28bb7a0f416bb09aca1b23d4b9e528d
Oscar Garcia <ogarcia2834@outlook.com>
2019-11-21 11:58:12 -0600

    mainscreen student color

--- GUI/GUI/mainscreen.Designer.cs
+++ GUI/GUI/mainscreen.Designer.cs
@@ -40,83 +40,83 @@
             // welcomelabel
             // 
             this.welcomelabel.AutoSize = true;
-            this.welcomelabel.Location = new System.Drawing.Point(18, 14);
-            this.welcomelabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.welcomelabel.ForeColor = System.Drawing.Color.White;
+            this.welcomelabel.Location = new System.Drawing.Point(12, 9);
             this.welcomelabel.Name = "welcomelabel";
-            this.welcomelabel.Size = new System.Drawing.Size(75, 20);
+            this.welcomelabel.Size = new System.Drawing.Size(52, 13);
             this.welcomelabel.TabIndex = 0;
             this.welcomelabel.Text = "Welcome";
             // 
             // studentnamelabel
             // 
             this.studentnamelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.studentnamelabel.Location = new System.Drawing.Point(105, 14);
-            this.studentnamelabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.studentnamelabel.ForeColor = System.Drawing.Color.White;
+            this.studentnamelabel.Location = new System.Drawing.Point(70, 9);
             this.studentnamelabel.Name = "studentnamelabel";
-            this.studentnamelabel.Size = new System.Drawing.Size(328, 20);
+            this.studentnamelabel.Size = new System.Drawing.Size(219, 13);
             this.studentnamelabel.TabIndex = 1;
             this.studentnamelabel.Click += new System.EventHandler(this.studentnamelabel_Click);
             // 
             // updatebutton
             // 
-            this.updatebutton.Location = new System.Drawing.Point(18, 52);
-            this.updatebutton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.updatebutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(184)))), ((int)(((byte)(206)))));
+            this.updatebutton.Location = new System.Drawing.Point(12, 34);
             this.updatebutton.Name = "updatebutton";
-            this.updatebutton.Size = new System.Drawing.Size(174, 35);
+            this.updatebutton.Size = new System.Drawing.Size(116, 23);
             this.updatebutton.TabIndex = 8;
             this.updatebutton.Text = "Update Information";
-            this.updatebutton.UseVisualStyleBackColor = true;
+            this.updatebutton.UseVisualStyleBackColor = false;
             this.updatebutton.Click += new System.EventHandler(this.updatebutton_Click);
             // 
             // enrolledclassesbutton
             // 
-            this.enrolledclassesbutton.Location = new System.Drawing.Point(201, 52);
-            this.enrolledclassesbutton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.enrolledclassesbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(184)))), ((int)(((byte)(206)))));
+            this.enrolledclassesbutton.Location = new System.Drawing.Point(134, 34);
             this.enrolledclassesbutton.Name = "enrolledclassesbutton";
-            this.enrolledclassesbutton.Size = new System.Drawing.Size(112, 35);
+            this.enrolledclassesbutton.Size = new System.Drawing.Size(75, 23);
             this.enrolledclassesbutton.TabIndex = 9;
             this.enrolledclassesbutton.Text = "Courses";
-            this.enrolledclassesbutton.UseVisualStyleBackColor = true;
+            this.enrolledclassesbutton.UseVisualStyleBackColor = false;
             this.enrolledclassesbutton.Click += new System.EventHandler(this.enrolledclassesbutton_Click);
             // 
             // gpabutton
             // 
-            this.gpabutton.Location = new System.Drawing.Point(321, 52);
-            this.gpabutton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gpabutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(184)))), ((int)(((byte)(206)))));
+            this.gpabutton.Location = new System.Drawing.Point(214, 34);
             this.gpabutton.Name = "gpabutton";
-            this.gpabutton.Size = new System.Drawing.Size(112, 35);
+            this.gpabutton.Size = new System.Drawing.Size(75, 23);
             this.gpabutton.TabIndex = 10;
             this.gpabutton.Text = "GPA";
-            this.gpabutton.UseVisualStyleBackColor = true;
+            this.gpabutton.UseVisualStyleBackColor = false;
             this.gpabutton.Click += new System.EventHandler(this.gpabutton_Click);
             // 
             // logoutbutton
             // 
-            this.logoutbutton.Location = new System.Drawing.Point(340, 97);
-            this.logoutbutton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.logoutbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(184)))), ((int)(((byte)(206)))));
+            this.logoutbutton.Location = new System.Drawing.Point(227, 63);
             this.logoutbutton.Name = "logoutbutton";
-            this.logoutbutton.Size = new System.Drawing.Size(112, 35);
+            this.logoutbutton.Size = new System.Drawing.Size(75, 23);
             this.logoutbutton.TabIndex = 11;
             this.logoutbutton.Text = "Logout";
-            this.logoutbutton.UseVisualStyleBackColor = true;
+            this.logoutbutton.UseVisualStyleBackColor = false;
             this.logoutbutton.Click += new System.EventHandler(this.logoutbutton_Click);
             // 
             // secretlabel
             // 
             this.secretlabel.AutoSize = true;
             this.secretlabel.ForeColor = System.Drawing.SystemColors.Control;
-            this.secretlabel.Location = new System.Drawing.Point(18, 105);
-            this.secretlabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.secretlabel.Location = new System.Drawing.Point(12, 68);
             this.secretlabel.Name = "secretlabel";
-            this.secretlabel.Size = new System.Drawing.Size(0, 20);
+            this.secretlabel.Size = new System.Drawing.Size(0, 13);
             this.secretlabel.TabIndex = 12;
             this.secretlabel.Visible = false;
             // 
             // mainscreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(471, 146);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(314, 95);
             this.Controls.Add(this.secretlabel);
             this.Controls.Add(this.logoutbutton);
             this.Controls.Add(this.gpabutton);
@@ -125,7 +125,6 @@
             this.Controls.Add(this.studentnamelabel);
             this.Controls.Add(this.welcomelabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "mainscreen";
             this.Text = "Main Screen";
             this.Load += new System.EventHandler(this.mainscreen_Load);
